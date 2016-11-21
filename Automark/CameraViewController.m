9881ce88b228deb214f2649d3b4462d388e75ff2
Melanie Hsu <hsumelanie@yahoo.com>
2016-11-21 17:38:45 -0500

    Saved picker image.

--- Automark/CameraViewController.m
+++ Automark/CameraViewController.m
@@ -44,6 +44,22 @@
     UIImage *thisImage = info[UIImagePickerControllerOriginalImage];
     self.camImageView.image = thisImage;
     [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+    // If photo taken by camera, write the photo to the Photo Library
+    if (picker.sourceType == UIImagePickerControllerSourceTypeCamera) {
+        UIImageWriteToSavedPhotosAlbum(thisImage, nil, nil, nil);
+    }
+    
+    // Save this photo so that it can be used by other parts of the app
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *imagePath = [documentsDirectory stringByAppendingPathComponent:@"original.png"];
+    
+    // Extract the image from the picker and save it
+    NSString *mediaType = [info objectForKey:UIImagePickerControllerMediaType];
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    NSData *imageData = UIImagePNGRepresentation(image);
+    [imageData writeToFile:imagePath atomically:YES];
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
